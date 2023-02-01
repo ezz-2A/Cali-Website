cb2b4b9c596dc223fac6e8c5f2a10bdcb5493d0c
ezz <122894798+ezz-2A@users.noreply.github.com>
2023-02-01 15:02:58 +0200

    avs

--- vr/js/vr.js
+++ vr/js/vr.js
@@ -34,3 +34,37 @@ setInterval(function(){
     // alert("gsldkfl")
 }
 });
+
+
+
+
+const myTimeout = setTimeout(myGreeting, 1000);
+        function myGreeting() {
+
+                $("#loogo").addClass("loogo1")
+                const myTimeout = setTimeout(myGreeting, 2500);
+        function myGreeting() {
+
+         $("#loogo").removeClass("loogo1")
+         
+        }
+         
+        }
+
+
+
+
+$("#loogo").hover(function(){
+
+   $("#loogo").addClass("loogo1")
+
+
+
+        const myTimeout = setTimeout(myGreeting, 2500);
+        function myGreeting() {
+
+         $("#loogo").removeClass("loogo1")
+         
+        }
+});
+
